9a1b42a116a4c4b2bc613a32c3d146df5559bef8
raissacosa <raissacosa@gmail.com>
2025-03-06 19:56:32 +0200

    feat: add User and Role entities for authentication feature

---
+++ src/main/java/com/raissac/budget_management/security/entity/Role.java
@@ -0,0 +1,5 @@
+package com.raissac.budget_management.security.entity;
+
+public enum Role {
+    ADMIN, USER
+}
---
+++ src/main/java/com/raissac/budget_management/security/entity/User.java
@@ -0,0 +1,39 @@
+package com.raissac.budget_management.security.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private LocalDate dateOfBirth;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+}
