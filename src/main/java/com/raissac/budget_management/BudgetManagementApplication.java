ffe3d94e4b69433ccaff7e7763a199f5188c1cb9
raissacosa <raissacosa@gmail.com>
2025-03-06 14:23:31 +0200

    chore: initial commit - setup Spring Boot project with dependencies: Spring Web, Spring Security, Spring Data JPA, PostgreSQL Driver, H2 Database, Lombok, Validation

---
+++ src/main/java/com/raissac/budget_management/BudgetManagementApplication.java
@@ -0,0 +1,13 @@
+package com.raissac.budget_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raissac/budget_management/BudgetManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raissac.budget_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
