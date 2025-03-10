37ca4761608b3bab1ac261c3c540872921df5256
raissacosa <raissacosa@gmail.com>
2025-03-10 12:03:14 +0200

    feat: implement login logic in UserService

--- src/main/java/com/raissac/budget_management/security/UserService.java
+++ src/main/java/com/raissac/budget_management/security/UserService.java
@@ -1,10 +1,15 @@
 package com.raissac.budget_management.security;
 
+import com.raissac.budget_management.security.config.JwtUtil;
+import com.raissac.budget_management.security.dto.AuthRequest;
 import com.raissac.budget_management.security.dto.RegisterRequest;
 import com.raissac.budget_management.security.entity.Role;
 import com.raissac.budget_management.security.entity.User;
 import com.raissac.budget_management.security.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -30,5 +35,18 @@ public class UserService {
                 .build();
 
         return userRepository.save(user);
+    }
+
+    public String login(AuthRequest request, AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.email(), request.password())
+        );
+
+        if (!authentication.isAuthenticated()) {
+            throw new RuntimeException("Invalid credentials!");
+        }
+
+        return jwtUtil.generateToken(request.email());
     }
 }
