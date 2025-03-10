aa6d13b0a62eb94ec5027ce48bb3b041378ef211
raissacosa <raissacosa@gmail.com>
2025-03-10 12:08:10 +0200

    feat: add login endpoint to AuthController

--- src/main/java/com/raissac/budget_management/security/controller/AuthController.java
+++ src/main/java/com/raissac/budget_management/security/controller/AuthController.java
@@ -2,6 +2,8 @@ package com.raissac.budget_management.security.controller;
 
 import com.raissac.budget_management.security.UserService;
 import com.raissac.budget_management.security.config.JwtFilter;
+import com.raissac.budget_management.security.config.JwtUtil;
+import com.raissac.budget_management.security.dto.AuthRequest;
 import com.raissac.budget_management.security.dto.RegisterRequest;
 import com.raissac.budget_management.security.entity.User;
 import jakarta.validation.Valid;
@@ -19,13 +21,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final UserService userService;
-    private final JwtFilter jwtFilter;
+    private final JwtUtil jwtUtil;
     private final AuthenticationManager authenticationManager;
 
     @PostMapping("/register")
     public ResponseEntity<String> register(@Valid @RequestBody RegisterRequest request) {
         userService.register(request);
         return ResponseEntity.ok("User registered successfully");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@Valid @RequestBody AuthRequest request) {
+        String token = userService.login(request, authenticationManager, jwtUtil);
+        return ResponseEntity.ok(token);
     }
 
 }
