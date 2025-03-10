7585ccf880848ba104b89a6cab06f2a4a09561de
raissacosa <raissacosa@gmail.com>
2025-03-10 13:23:33 +0200

    fix: update jwt.secret for HS256 and configure security to allow unauthenticated access to login

--- src/main/java/com/raissac/budget_management/security/config/SecurityConfig.java
+++ src/main/java/com/raissac/budget_management/security/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
 
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests( auth -> auth
-                        .requestMatchers("api/v1/auth/register").permitAll()
+                        .requestMatchers("/api/v1/auth/**").permitAll()
                         .anyRequest().authenticated())
                 .sessionManagement( session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
